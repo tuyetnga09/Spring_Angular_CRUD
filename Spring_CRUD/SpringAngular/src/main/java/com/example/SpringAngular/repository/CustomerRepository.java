42e2508885c021fe36b0e2b76c71af2d2ec346af
tuyetnga09 <ngapttph23404@fpt.edu.vn>
2023-04-24 22:49:12 +0700

    Create CustomerRepository.java

---
+++ Spring_CRUD/SpringAngular/src/main/java/com/example/SpringAngular/repository/CustomerRepository.java
@@ -0,0 +1,12 @@
+package com.example.SpringAngular.repository;
+
+import com.example.SpringAngular.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Repository;
+
+@EnableJpaRepositories
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+
+}
